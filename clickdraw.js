19f537bce45d2bae0b957c63ab2677e3ca70d2d1
Bayan Berri <bberri@stuy.edu>
2018-02-04 22:04:05 -0500

    Donezo

--- clickdraw.js
+++ clickdraw.js
@@ -1,21 +1,50 @@
-var c=document.getElementById("slate");//gets the canvas tag 
+
+var c=document.getElementById("slate"); //gets the canvas tag 
 var ctx=c.getContext("2d");//obj info
-ctx.fillStyle="cyan";
-ctx.strokeStyle="black";//changes the color
-ctx.fillRect(50,50,100,200);//
-/*ctx.clearRect(50,50,100,200);
-for (var i = 0; i < 6; i++){
-  for (var j = 0; j < 6; j++){
-    ctx.fillStyle = 'rgb(' + Math.floor(255 - 42.5 * i) + ',' +
-                     Math.floor(255 - 42.5 * j) + ',0)';
-    ctx.fillRect(j * 25, i * 25, 25, 25);
-  }
+//Getting the button values
+var clear=document.getElementById("clear");
+var toggle=document.getElementById("toggle");
+var shape= true; // let true be circle, false is square
+
+//this draws the circle with an outline of black and a fill color of cyan
+var circle= function(x,y){
+  ctx.strokeStyle="black";
+  ctx.fillStyle="cyan";
+  ctx.beginPath();
+  ctx.arc(x-10,y-10,10,0,2*Math.PI);//subtracting to center the mouse click
+  ctx.stroke();
+  ctx.fill();
 }
 
-/*
-fillStyle()
-strokeStyle()
-clearRect()
-fillText()
-
-*/
+//draws the square with an ourline of red and a fill color of purple
+var square=function(x,y){
+  ctx.strokeStyle="red";
+  ctx.fillStyle="purple";
+  ctx.fillRect(x-15,y-15,15,15);//subtracting to center the mouse click
+}
+//if the toggle button is clicked this negates the shape.
+var changeShape=function(){
+  if(shape){
+    shape=false;
+  }
+  else{
+    shape=true;
+  }
+}
+// drawShape is called at mouse click and calls the circle and square functions
+var drawShape=function(e){
+  ctx.fillStyle="cyan";
+  if(shape){
+    circle(e.x,e.y);//at the mouse location
+  }
+  else{
+    square(e.x,e.y);
+  }
+}
+//when clear is clicked it clears the canvas, all coordinates.
+var clearCanvas=function(){
+  ctx.clearRect(0,0,600,600);
+}
+c.addEventListener("click",drawShape);
+toggle.addEventListener("click",changeShape);
+clear.addEventListener("click",clearCanvas);
